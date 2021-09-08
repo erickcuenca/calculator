31c50c35ed5666c9f81c410056443866f4b995d0
erickcuenca <79394189+erickcuenca@users.noreply.github.com>
2021-09-08 11:25:25 -0700

    calculator fully complete with all functions an operands working

--- scripts/index.js
+++ scripts/index.js
@@ -5,18 +5,146 @@ const add = document.querySelector('[data-operator="+"]');
 const subtract = document.querySelector('[data-operator="-"]');
 const equals = document.querySelector('[data-operator="="]');
 const operators = calculator.querySelectorAll("[data-operator]");
+const digits = calculator.querySelectorAll("[data-digit]");
+const functions = calculator.querySelectorAll("[data-function]");
+const operation = document.querySelector("#operation");
+const lastValue = document.querySelector("#lastValue");
+const currentValue = document.querySelector("#currentValue");
 let operatorChoice;
+lastValue.textContent = ""
+currentValue.textContent = "0"
+let digitCount;
+//digits
+function checkIfOperatorIsNotSelected() {
+    if(!divide.classList.contains("operatorSelected") &&
+    !multiply.classList.contains("operatorSelected") &&
+    !add.classList.contains("operatorSelected") &&
+    !subtract.classList.contains("operatorSelected")) {
+        return true
+    }
+    else {
+        return false
+    }
+}
 
+//creating a new value
+digits.forEach(e => {
+    e.addEventListener("pointerdown", function() {
+        console.log(e.textContent);
+        removeOperatorSelection();
+        digitCount = currentValue.textContent.length;
+        if(currentValue.textContent.includes("-")) {
+            --digitCount;
+        }
+        if(currentValue.textContent.includes(".")) {
+            --digitCount;
+        }
+        if(digitCount <= 7) {
+            if(currentValue.textContent === "0" && e.textContent != ".") {
+                currentValue.textContent = "";
+                currentValue.textContent += e.textContent;
+            }
+            else if(currentValue.textContent === "-0" && e.textContent === "0") {}
+            else if(currentValue.textContent === "-0" && e.textContent === ".") {
+            currentValue.textContent += e.textContent;
+            }
+            else if(currentValue.textContent ==="-0" && e.textContent != "0") {
+                currentValue.textContent = "-";
+                currentValue.textContent += e.textContent;
+            }
+            else if(e.textContent === "." && currentValue.textContent.includes(".")){}
+            else {
+            currentValue.textContent += e.textContent;
+            }
+       }
+       else {}
+    }) 
+})
+//calculate
+function calculate() {
+    if(lastValue.textContent === "") {
+        if(operatorChoice === "%" && parseFloat(currentValue.textContent) != 0) {
+            removeOperatorSelection();
+            operation.textContent = "";
+            lastValue.textContent = parseFloat(currentValue.textContent) / 10;
+        }
+    }
+    else {
+        if(operatorChoice === "÷"){
+            lastValue.textContent = parseFloat(lastValue.textContent) / parseFloat(currentValue.textContent);
+        }
+        else if(operatorChoice === "x"){
+            lastValue.textContent = parseFloat(lastValue.textContent) * parseFloat(currentValue.textContent);
+        }
+        else if(operatorChoice === "-"){
+            lastValue.textContent = parseFloat(lastValue.textContent) - parseFloat(currentValue.textContent);
+        }
+        else if(operatorChoice === "+"){
+            lastValue.textContent = parseFloat(lastValue.textContent) + parseFloat(currentValue.textContent);
+        }    
+        else if(operatorChoice === "%") {
+            removeOperatorSelection();
+            operation.textContent = "";
+            lastValue.textContent = parseFloat(lastValue.textContent) / 10;
+        }
+        else if(operatorChoice === "=") {
+            if(operation.textContent === "") {
+                lastValue.textContent = currentValue.textContent;
+            }
+            else {
+            operation.textContent = ""
+            }
+        }
+        else {
+            operation.textContent = this.innerText;
+        }
+    }
+}   
+
+//saving values
+function saveValue() {
+    lastValue.textContent = currentValue.textContent;
+    currentValue.textContent =  "0";
+}
+//operators
 function removeOperatorSelection() {
     operators.forEach(e => {
-        e.classList.remove("operatorSelected")
-        console.log(e);
+        e.classList.remove("operatorSelected");
     })
 }
 operators.forEach(e => {
     e.addEventListener("pointerdown", function() {
+        if(!checkIfOperatorIsNotSelected()) {
+        }
+        else {
+            if(lastValue.textContent === "") {
+                saveValue();
+            }
+            else {
+                calculate();
+                if(currentValue.textContent.includes("-")){
+                    currentValue.textContent =  "0";
+                }
+                else {
+                    currentValue.textContent =  "0";
+                }
+            }
+        }
         removeOperatorSelection();
+        operation.textContent = this.innerText;
         operatorChoice = this.innerText;
+        if(operatorChoice === "=") {
+            if(operation.textContent === "") {
+                lastValue.textContent = currentValue.textContent;
+            }
+            else {
+            operation.textContent = ""
+            }
+        }
+        else {
+            operation.textContent = this.innerText;
+        }
+        //operation functions
         if(operatorChoice === "÷") {
             divide.classList.add("operatorSelected");
         }
@@ -29,8 +157,32 @@ operators.forEach(e => {
         else if(operatorChoice === "+") {
             add.classList.add("operatorSelected");
         }
-        else if(operatorChoice === "=") {
-            
-        }
+        
     });
+})
+
+//functions
+functions.forEach(e => {
+    e.addEventListener("pointerdown", function() {
+        if(e.innerText === "AC") {
+            removeOperatorSelection();
+            lastValue.textContent = "";
+            currentValue.textContent = "0";
+            operation.textContent = "";
+        }
+        else if(e.innerText === "±") {
+            removeOperatorSelection();
+            if(currentValue.textContent.includes("-")) {
+                currentValue.textContent = currentValue.textContent.slice(1);
+            }
+            else {
+                currentValue.textContent = "-" + currentValue.textContent;
+            }
+        }
+        else if(e.innerText === "%"){
+            operatorChoice = "%";
+            calculate();
+            currentValue.textContent = "0"
+        }
+    })
 })
